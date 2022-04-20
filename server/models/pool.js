cbc08d96d99a8d6e6bffa07222c49460c80deb06
Ocean <oeverspe@uncc.edu>
2022-04-20 16:48:26 +0900

    preparing for heroku

--- server/models/pool.js
+++ server/models/pool.js
@@ -1,12 +1,32 @@
 import pg from 'pg'
+import dotenv from 'dotenv'
+
 const { Pool } = pg
 
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'loyalty_db',
-  password: 'password',
-  port: 5432
-})
+dotenv.config()
+const { NODE_ENV } = process.env
+
+let pool = null
+
+if (NODE_ENV == 'development') {
+  const {DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT} = process.env
+
+  pool = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    port: DB_PORT
+  })
+}
+
+if (NODE_ENV == 'production') {
+  const {DATABASE_URL} = process.env
+
+  pool = new Pool({
+    connectionString: DATABASE_URL
+  })
+
+}
 
 export default pool
